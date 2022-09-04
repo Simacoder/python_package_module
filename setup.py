da0300dec3d2630c8bc4aa172ea7e25917b81d4d
Simanga <simacoder@hotmail.com>
2022-09-04 19:31:44 +0200

    package fixed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name = 'distribution',
-	  version = '0.2',
+setup(name = 'simangadistribution',
+	  version = '1.3',
 	  description = 'Gaussian distribution',
 	  packages = ['binomial_package'],
 	  zip_safe = False)
