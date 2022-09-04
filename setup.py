654feb9ed3946301bf8bbef15b47104243970572
Simanga <simacoder@hotmail.com>
2022-09-04 16:17:56 +0200

    AWS machine larning package development;

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'distribution',
+	  version = '0.2',
+	  description = 'Gaussian distribution',
+	  packages = ['binomial_package'],
+	  zip_safe = False)
